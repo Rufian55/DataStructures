1382595f962604f07af0ad3f976f0fa9beba6935
Chris Kearns <kearnsc@oregonstate.edu>
2016-08-23 12:22:57 -0400

    Update PassByVal_Ref.c

--- Easy C Programs/PassByVal_Ref.c
+++ Easy C Programs/PassByVal_Ref.c
@@ -1,5 +1,4 @@
-/* CS261- Assignment 1 - Q.2 */
-/* Name: Chris Kearns
+/* Author: Chris Kearns
  * Date: 10 April 2016
  * Solution description: Demo of C behaviour for pass by value vs.
  * pass by reference (pointers).
@@ -13,36 +12,35 @@ int foo(int* a, int* b, int c){
 	*a *= 2;
 	/*Set b to half its original value*/
 	*b /= 2;
-    /*Assign a+b to c*/
+    	/*Assign a+b to c*/
 	c = *a + *b;
-    /*Return c*/
-	return c;
+    	return c;
 }
 
 int main(){
-	/*Declare three integers x,y and z and initialize them to 5, 6, 7 respectively*/
+	/* Declare three integers x,y and z and initialize them to 5, 6, 7 respectively */
 	int x = 5;
 	int y = 6;
 	int z = 7;
 	int result;
 
-	/*Print the values of x, y and z*/
+	/* Print the values of x, y and z */
 	printf("x = %i\ny = %i\nz = %i\n", x, y, z);
 
-	/*Call foo() appropriately, passing x,y,z as parameters*/
+	/* Call foo() appropriately, passing x,y,z as parameters */
 	result = foo(&x, &y, z);
 
-	/*Print the value returned by foo*/
+	/* Print the value returned by foo */
 	printf("Returned value from foo = %i\n", result);
 
-	/*Print the values of x, y and z again*/
+	/* Print the values of x, y and z again */
 	printf("x = %i\ny = %i\nz = %i\n", x, y, z);
 
-	/*Is the return value different than the value of z?  Why?*/
+	/* Is the return value different than the value of z?  Why? */
     return 0;
 }
 /*
-No, z (equal to 7) was passed to foo by value, (which means a copy of z was passed to foo), not
-by reference like x and y were done.  As such, the assignment operation at line 17 was performed
+Note: z (equal to 7) was passed to foo by value, (which means a copy of z was passed to foo), not
+by reference like x and y were done.  As such, the assignment operation was performed
 on the copy of z, and that was then returned, leaving the original z in main unchanged.
 */
